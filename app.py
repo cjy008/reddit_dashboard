42b891ef75c791dd10002ead59d19d8010331bfc
Cindy Yu <cjy008@ucsd.edu>
2020-03-21 22:01:25 -0700

    App is able to pull from the reddit API now

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import praw
 import pandas as pd
+from pprint import pprint
 
-#from config import cid, csec, ua
+from config import cid, csec, ua
 
 #create a reddit connection
 reddit = praw.Reddit(client_id= cid,
                      client_secret= csec,
                      user_agent= ua)
+
+subreddits_titles = ["news", "worldnews", "science", "coronavirus"]
+
+for sub in subreddits_titles:
+	subreddit = reddit.subreddit(sub).top(limit=1)
+
+	for post in subreddit:
+		pprint(vars(post))
