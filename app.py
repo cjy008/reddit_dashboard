ce24e175decf20e1dd40b570517736f941db1ea7
melody.yu <melody.yu12@gmail.com>
2020-03-21 22:12:41 -0700

    Print individual attributes

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ from config import cid, csec, ua
 
 #create a reddit connection
 reddit = praw.Reddit(client_id= cid,
-                     client_secret= csec,
-                     user_agent= ua)
+     client_secret= csec,
+     user_agent= ua)
 
+#chosen ubreddits
 subreddits_titles = ["news", "worldnews", "science", "coronavirus"]
 
+#return attributes top posts in subreddits of interest
 for sub in subreddits_titles:
-	subreddit = reddit.subreddit(sub).top(limit=1)
+     subreddit = reddit.subreddit(sub).top(limit=1)
 
-	for post in subreddit:
-		pprint(vars(post))
+     for post in subreddit:
+    #pprint(vars(post))
+        print(post.title)
+        print(post.score)
+        print(post.num_comments)
+        print(post.selftext)
+        print(post.created)
+        print(post.total_awards_received)
